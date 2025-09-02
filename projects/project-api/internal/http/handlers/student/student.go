41dffb0d6b3579d805a235285d300d019316a84a
Mayuraksha Sikdar <mayurakshasikdar@gmail.com>
2025-09-02 12:47:47 +0100

    project 1 - added validation

---
+++ projects/project-api/internal/http/handlers/student/student.go
@@ -0,0 +1,39 @@
+package student
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/MayurakshaSikdar/golang-tutorial/project-api/internal/types"
+	"github.com/MayurakshaSikdar/golang-tutorial/project-api/internal/utils/response"
+	"github.com/go-playground/validator/v10"
+)
+
+func New() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("Creating a student...")
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+		if err := validator.New().Struct(student); err != nil {
+			validateErrors := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrors))
+			return
+		}
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+	}
+}
